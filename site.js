76654a06b2cf130413d1d6fd0305ddb9ea0e0657
Priyanka Gupta <priyanka@webcontentor.com>
2016-07-14 17:18:24 +0530

    hamburger functionality added

--- site.js
+++ site.js
@@ -1,27 +1,96 @@
 $(document).ready(function(){
+    $("#hamburger").hide();
+    $("#img").hide();
+    var hamelec = false;
+    var hamauto = false;
+    var hamclothe = false;
+    var elec = true;
+    var auto = true;
+    var clothe = true;
+    var pin = false;
+
     $("#clothing").hide();
     $("#autoMobiles").hide();
     $("#electronic").hide();
-    $("#sam").hide();
-    $("#apple").hide();
-    $("#nokia").hide();
-    $("#mob").hide();
     $("#para").hide();
+    
+    $("#img").click(function(){
+        if (pin == false) {
+            pin = true;
+            $("#img").addClass("pin");
+        }else{
+            $("#img").removeClass("pin");
+            pin = false;
+        }
+    });
 
-    $("#hamburger").click(function(){
-        $("#electronic").hide();
-    });
     $("#1").click(function(){
-        $("#electronic").show();
-        $("#mob").show();
+        $("#hamburger").show();
+        $("#img").show();
+        if (elec) {
+            elec = false;
+            $("#electronic").show();
+            $("#autoMobiles").hide();
+            $("#clothing").hide();
+        }else{
+            return;
+        }
     });
-    $("#mob").click(function(){
-        $("#sam").show();
-        $("#apple").show();
-        $("#nokia").show();
+
+    $("#2").click(function(){
+        $("#hamburger").show();
+        if (auto) {
+           auto =  false;
+            $("#autoMobiles").show();
+            $("#electronic").hide();
+            $("#clothing").hide();
+        }else{
+            return;
+        }
     });
+
+    $("#3").click(function(){
+        $("#hamburger").show();
+        if (clothe) {
+            clothe = false;
+            $("#clothing").show();
+            $("#electronic").hide();
+            $("#autoMobiles").hide();
+       }else{
+            return;
+       }
+    });
+   
     $("#sam").click(function(){
         $("#para").show();
         console.log(para);
     });
+
+    $("#hamburger").click(function(){
+        if (hamelec == false) {
+            $("#electronic").hide();
+            hamelec = true;
+        }else if(hamelec) {
+            $("#electronic").show();
+            $("#autoMobiles").hide();
+            $("#clothing").hide();
+            hamelec = false;
+        }else if (hamauto == false) {
+            $("#autoMobiles").hide();
+            hamauto = true;
+        }else if(hamauto){
+            $("#autoMobiles").show();
+            $("#electronic").hide();
+            $("#clothing").hide();
+            hamauto = false;
+        }else if (hamclothe == false) {
+            $("#clothing").hide();
+            hamclothe = true;
+        }else{
+            $("#clothing").show();
+            $("#electronic").hide();
+            $("#autoMobiles").hide();
+            hamclothe = true;
+        }
+    });  
 });
