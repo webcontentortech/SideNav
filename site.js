6fb9b6753f1b4c82c50a2a2fba5512072b4c38ad
Priyanka Gupta <priyanka@webcontentor.com>
2016-07-23 18:15:04 +0530

    create navigation

--- site.js
+++ site.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     var pin = false;
     var ham = true;
     var angle = true;
+    var i = 1;
 
     var hamburger = function(){
         ham = !ham;
@@ -20,6 +21,7 @@ $(document).ready(function(){
     }
 
     $("#hamburger").click(hamburger);
+
     $("#firstHeading").click(function(){
         $("#hamburger").show();
         $("#img").show();
@@ -77,7 +79,7 @@ $(document).ready(function(){
     });
 
     $("#mob").click(function(){
-        if(angle){
+       if(angle){
             angle = false;
             $("#angle1").addClass("angle");
             $("#mobItems").hide();
@@ -98,16 +100,95 @@ $(document).ready(function(){
             $("#tvItems").show();
             angle = true;
         }
-    });    
+    });
+
+    $("#lap").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle3").addClass("angle");
+            $("#lapItems").hide();
+        }else{
+            $("#angle3").removeClass("angle");
+            $("#lapItems").show();
+            angle = true;
+        }
+    });
+
+    $("#cars").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle4").addClass("angle");
+            $("#carItems").hide();
+        }else{
+            $("#angle4").removeClass("angle");
+            $("#carItems").show();
+            angle = true;
+        }
+    });
+
+    $("#bikes").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle5").addClass("angle");
+            $("#bikeItems").hide();
+        }else{
+            $("#angle5").removeClass("angle");
+            $("#bikeItems").show();
+            angle = true;
+        }
+    });
+
+    $("#truck").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle6").addClass("angle");
+            $("#truckItems").hide();
+        }else{
+            $("#angle6").removeClass("angle");
+            $("#truckItems").show();
+            angle = true;
+        }
+    });
+
+    $("#sari").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle7").addClass("angle");
+            $("#sareesItems").hide();
+        }else{
+            $("#angle7").removeClass("angle");
+            $("#sareesItems").show();
+            angle = true;
+        }
+    });
+
+    $("#zeans").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle8").addClass("angle");
+            $("#zeansItems").hide();
+        }else{
+            $("#angle8").removeClass("angle");
+            $("#zeansItems").show();
+            angle = true;
+        }
+    });
+
+    $("#tshirt").click(function(){
+        if(angle){
+            angle = false;
+            $("#angle9").addClass("angle");
+            $("#tshirtItems").hide();
+        }else{
+            $("#angle9").removeClass("angle");
+            $("#tshirtItems").show();
+            angle = true;
+        }
+    });
 }); 
 
 var getElement = function(element,elem){
     var element = element.text();
     var elem = elem.text();
     $("#para").text("This is" +" " +element+" "+elem+" "+ "page");
-}
-
-var getId = function(){
-    var b = $(this).attr("id");
-    console.log(b);
 }
